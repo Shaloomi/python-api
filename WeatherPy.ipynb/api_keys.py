b13dcff23dfc2126dd5c9720f7bd3393acefb2de
Shaloomi <110343234+Shaloomi@users.noreply.github.com>
2022-09-19 13:40:51 +1000

    Update api_keys.py

--- WeatherPy.ipynb/api_keys.py
+++ WeatherPy.ipynb/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "98dc7c74302439927f8f524f064b4c82"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAfruh1IbUSaCFXPx_fQQUCxz7HsVhXzrc"
+g_key = ""
